f18ec1026875eb0656acf689286baa60ba5be0d9
Kate Sant <kate.sant@yahoo.com>
2023-02-02 18:59:19 +0000

    CR-17 Protect API with auth - Oauth resource server

--- src/main/java/com/thinktalkbuild/contractreader/engine/ContractreaderEngine.java
+++ src/main/java/com/thinktalkbuild/contractreader/engine/ContractreaderEngine.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContractreaderApplication {
+public class ContractreaderEngine {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContractreaderApplication.class, args);
+		SpringApplication.run(ContractreaderEngine.class, args);
 	}
 
 }
