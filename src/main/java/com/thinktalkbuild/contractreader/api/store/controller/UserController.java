29cac2aa36f112d7c8c4b58a362701f5597994b8
Kate Sant <kate.sant@yahoo.com>
2023-02-12 10:57:37 +0000

    CR-21 Extract unique user ID from jwt token

--- src/main/java/com/thinktalkbuild/contractreader/api/store/controller/UserController.java
+++ src/main/java/com/thinktalkbuild/contractreader/api/store/controller/UserController.java
@@ -1,23 +1,12 @@
 package com.thinktalkbuild.contractreader.api.store.controller;
 
-import com.thinktalkbuild.contractreader.api.engine.model.Analysis;
-import com.thinktalkbuild.contractreader.api.engine.model.ContractSummary;
-import com.thinktalkbuild.contractreader.api.engine.model.Duration;
-import com.thinktalkbuild.contractreader.api.engine.model.ObligationsByParty;
-import com.thinktalkbuild.contractreader.api.engine.service.ContractSummariser;
-import com.thinktalkbuild.contractreader.api.engine.service.DurationFinder;
-import com.thinktalkbuild.contractreader.api.engine.service.ObligationsFinder;
-import com.thinktalkbuild.contractreader.api.engine.service.WordDocReader;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.List;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 /**
  *
@@ -31,6 +20,13 @@ public class UserController {
     @PostMapping("/user")
     public void user(){
 
+        log.info("Call to /user POST endpoint");
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        String username = authentication.getName();
+        Jwt principal = (Jwt)authentication.getPrincipal();
+
+        log.info("Principal subject = [{}]", principal.getSubject());
 
     }
 }
